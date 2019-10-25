f2f6d554d401edc949cc4ff82897c17b5e1fbbad
ogeindev <ogeindev@hotmail.com>
2019-10-25 07:05:52 +0200

    forumEdit completed and some style

--- backend-nodejs-mysql/src/routes/profile.js
+++ backend-nodejs-mysql/src/routes/profile.js
@@ -4,29 +4,18 @@ const pool = require('../database')
 
 // edit user info
 router.put('/editUser', async(req, res) => {
-        let { username, userage, useremail, id_user } = req.body;
-        let newInfUser = { username, userage, useremail, id_user };
-        await pool.query('UPDATE users set ? Where id_user = ?', [newInfUser, id_user])
-        console.log('one user edited', newInfUser)
-    })
-    // get articles by id
-router.get('/getUserArticles', async(req, res) => {
-        let userArticles = await pool.query('SELECT * FROM articles ORDER BY data_created DESC')
-        res.send(userArticles)
-        console.log('userArticles onloaded')
-    })
-    // get forumposts by id
+    let { username, userage, useremail, id_user } = req.body;
+    let newInfUser = { username, userage, useremail, id_user };
+    await pool.query('UPDATE users set ? Where id_user = ?', [newInfUser, id_user])
+    console.log('one user edited', newInfUser)
+})
+
+////////////FORUM ////////////////
+// get forumposts by id
 router.get('/getUserForumposts', async(req, res) => {
         let userForumposts = await pool.query('SELECT * FROM forumposts ORDER BY data_created DESC')
         res.send(userForumposts)
         console.log('Forumposts onloaded')
-    })
-    //delete article
-router.delete('/deleteArticle/:id', async(req, res) => {
-        const { id } = req.params;
-        let articleDeleted = await pool.query('DELETE FROM articles WHERE id_article = ?', [id])
-        res.send(articleDeleted)
-        console.log('Article Deleted', articleDeleted)
     })
     //delete forumpost
 router.delete('/deleteForumpost/:id', async(req, res) => {
@@ -34,6 +23,37 @@ router.delete('/deleteForumpost/:id', async(req, res) => {
         let forumpost = await pool.query('DELETE FROM forumposts WHERE id_forumpost = ?', [id])
         res.send(forumpost)
         console.log('Forumpost Deleted', articleDeleted)
+    })
+    //get one forumpost
+router.get('/forumpost/:id', async(req, res) => {
+        const { id } = req.params;
+        let forumpost = await pool.query('SELECT * FROM forumposts WHERE id_forumpost = ?', [id])
+        res.send(forumpost)
+        console.log('Article Selected', forumpost)
+    })
+    //post article edited
+router.put('/saveForumpost', async(req, res) => {
+    let { titleforumpost, textforumpost, autorforumpost, categoryforumpost, tagsforumpost, id_user, id_forumpost } = req.body;
+    let forumpostSaved = { titleforumpost, textforumpost, autorforumpost, categoryforumpost, tagsforumpost, id_user, id_forumpost };
+    await pool.query('UPDATE forumposts set ? Where id_forumpost = ?', [forumpostSaved, id_forumpost])
+    console.log('one forumpost edited', forumpostSaved)
+})
+
+
+
+//////////// ARTICLEs //////////////
+// get articles by id
+router.get('/getUserArticles', async(req, res) => {
+        let userArticles = await pool.query('SELECT * FROM articles ORDER BY data_created DESC')
+        res.send(userArticles)
+        console.log('userArticles onloaded')
+    })
+    //delete article
+router.delete('/deleteArticle/:id', async(req, res) => {
+        const { id } = req.params;
+        let articleDeleted = await pool.query('DELETE FROM articles WHERE id_article = ?', [id])
+        res.send(articleDeleted)
+        console.log('Article Deleted', articleDeleted)
     })
     //get one article
 router.get('/article/:id', async(req, res) => {
@@ -44,11 +64,10 @@ router.get('/article/:id', async(req, res) => {
     })
     //post article edited
 router.put('/saveArticle', async(req, res) => {
-    // let { id } = req.params;
     let { titlearticle, textarticle, autorarticle, categoryarticle, tagsarticle, id_user, id_article } = req.body;
     let articleSaved = { titlearticle, textarticle, autorarticle, categoryarticle, tagsarticle, id_user, id_article };
     await pool.query('UPDATE articles set ? Where id_article = ?', [articleSaved, id_article])
-    console.log('one user edited', articleSaved)
+    console.log('one article edited', articleSaved)
 })
 
 
--- frontend-vue/src/views/Profile.vue
+++ frontend-vue/src/views/Profile.vue
@@ -3,9 +3,10 @@
     <div class="container-fluid">
       <div class="row">
         
-        <div class="col-lg-12 bg-style m-1 margtop">
-          <h1>Hello {{user.username}}!!</h1>
+        <div class="col-lg-11 bg-style m-1 margtop">
+          <h1>Welcome to your profile {{user.username}}!</h1>
         </div>
+      <!--    start          DIV USERDATA              -->
       <div v-show="personalData" class="col-lg-6 bg-style m-1 ml-3 p-1 divsquery leftdiv">    
           <h2 class="bg-white">Personal data</h2>
           <div class="t-align-left">
@@ -19,48 +20,105 @@
             <button v-on:click="editProfile"  class="btn btn-success float-right ml-2 buttonEdit">Edit</button>
           </div>
       </div>
+        <!--    end          DIV USERDATA               -->
+
+
+        <!--    start          DIV FRIENDS              -->
         <div  v-show="friendsData" class="col-lg-5 bg-style m-1 p-1 divsquery ">
           <h2 class="bg-white">Friends</h2>
           <div class="t-align-left">
-            <h3>Connected</h3>
+            <h3 class="bg-green2">Connected</h3>
             <div>
               <p>sdsds</p>
               <p>dsdsd</p>
             </div>
           </div>
           <div class="t-align-left">
-            <h3>Disconnected</h3>
+            <h3 class="bg-green2">Disconnected</h3>
             <p>sdsdsds</p>
             <p>dsdsdsds</p>
           </div>
         </div>
+        <!--    end          DIV FRIENDS               -->
 
+
+        <!--    start          DIV FORUMPOSTS               -->
         <div v-show="postsData" class="col-lg-6 bg-style m-1 ml-3 p-1 divsquery leftdiv">
           <h2 class="bg-white">Posts</h2>
           <div class="t-align-left">
-            <h2 class="bg-white2">My posts</h2>          
+            <h2 class="bg-green2">My posts</h2>          
             <div class="scrollArticles">
 
               <div v-for="userForumpost in userForumposts" v-bind:key="userForumpost.id_forumpost" >
                 <div>
                   <p> {{userForumpost.titleforumpost}} </p>
-                  <button class="btn barticle">edit</button>
+                  <button @click="editForumpost(userForumpost.id_forumpost)" class="btn barticle">edit</button>
                   <button @click="deleteForumpost(userForumpost.id_forumpost)" class="btn barticle">delete</button>
                 </div>
               </div>  
             </div>
+            <button v-show="backButtonForum"  @click="backButtonForumpost"  class="btn btn-success btn-block mt-1">Back</button>
           </div>
-          <div class="t-align-left">
-            <h2>My favorite posts</h2>
+          <div v-show="postsFavData" class="t-align-left">
+            <h2 class="bg-green2">My favorite posts</h2>
             <p>sdsdsds</p>
             <p>dsdsdsds</p>
           </div>
         </div>
+        <!--              DIV FORUMPOSTS      end        -->
 
+
+        <!-- start EDIT Forum -->
+        <div v-show="forumpostInf" class="col-lg-12">
+
+          <div class="card bg-style margArt">
+            <div class="card-body">
+              <form >
+                <div class="form-group">
+                  <strong>Title:</strong>
+                  <input type="text" class="form-control" v-model="forumpost.titleforumpost" autofocus>
+        
+                </div>
+              
+                <div class="form-group">
+                  <strong>Category:</strong>                 
+                  <select class="custom-select" v-model="forumpost.categoryforumpost">
+                    <option disabled value> Select one category</option>
+                    <option v-for="category in categories" v-bind:key="category">
+                      {{category}}
+                    </option>
+                  </select>                
+                </div>
+                <template>
+                  <div class="form-group">
+                    <strong>Tags:</strong>
+                    <vue-tags-input
+                      v-model="forumpost.tagsforumpost"  
+                      :tags="tags"
+                      @tags-changed="newTags => tags = newTags"
+                    />
+                  </div>
+                </template>                
+                <div class="form-group">
+                  <strong>Text:</strong>    
+                  <textarea v-model="forumpost.textforumpost" rows="8" class="form-control" placeholder="Text"></textarea>
+                </div>
+                <div class="form-group">
+                  <button  @click="saveForumpost" type="submit" class="btn btn-success btn-block">Save</button>
+                  {{error}}
+                </div>            
+              </form>
+            </div>
+          </div>
+        </div>
+        <!-- end   EDIT Forum -->
+        
+
+        <!--      start        DIV ARTICLES               -->
         <div v-show="articlesData" class="col-lg-5 bg-style m-1 p-1 divsquery">
           <h2 class="bg-white">Articles</h2>
           <div class="col t-align-left">
-            <h3 class="bg-white2">My articles</h3>
+            <h3 class="bg-green2">My articles</h3>
             <div class="scrollArticles">
 
               <div v-for="userArticle in userArticles" v-bind:key="userArticle.id_article" >
@@ -71,15 +129,18 @@
                 </div>
               </div>  
             </div>
+            <button v-show="backButtonArt"  @click="backButtonArticle"  class="btn btn-success btn-block mt-1">Back</button>
           </div>
           <div v-show="articlesFavData" class="col t-align-left">
-            <h3>My favorite articles</h3>
+            <h3 class="bg-green2">My favorite articles</h3>
             <p>sdsdsds</p>
             <p>dsdsdsds</p>
           </div>
         </div>
-        
-        <!-- Edit info -->
+        <!--      end        DIV ARTICLES               -->
+
+
+        <!-- start EDIT user -->
         <div 
        v-show="profileInf" class="col-lg-5 bg-style m-1 p-1 divsquery  editUser" id="way">
           <h2 class="bg-white">Edit your profile!</h2>
@@ -115,16 +176,13 @@
             </div>
           </div>
         </div>
+        <!-- end EDIT user -->
+      
 
-        <!-- Edit info -->
+    <!-- start EDIT Article -->
+    <div v-show="articleInf" class="col-lg-12">
 
-      </div>
-    </div>
-  
-    <!-- Edit Article -->
-    <div v-show="articleInf" class="col-lg-12 ">
-
-      <div class="card bg-warning margArt">
+      <div class="card bg-style margArt">
         <div class="card-body">
           <form >
             <div class="form-group">
@@ -164,11 +222,15 @@
         </div>
       </div>
     </div>
-    <!-- Edit Article -->
-    <pre>{{user.id_user}}</pre>
+    <!-- end   EDIT Article -->
+      </div>
+    </div>
+  
+
+    <!-- <pre>{{user.id_user}}</pre>
     <pre>{{article.tagsarticle}}</pre>
     <pre>{{tags}}</pre>
-    <pre>{{article}}</pre>
+    <pre>{{article}}</pre> -->
 
   </div>
 </template>
@@ -176,6 +238,7 @@
 <script>
 import axios from "axios";
 import VueTagsInput from '@johmun/vue-tags-input';
+
 export default {
   components: {
     VueTagsInput,
@@ -184,33 +247,42 @@ export default {
   data() {
     return {
       user: "",
+      // STATUS
       personalData: true,
+      profileInf: false,
       friendsData: true,
       postsData: true,
+      postsFavData: true,
+      forumpostInf :false,
       articlesData: true,
       articlesFavData: true,
-      profileInf: false,
       articleInf: false,
+      // STATUS BUTTON
+      backButtonArt: false,
+      backButtonForum: false,
+
+      // TO CHANGE!
       usernameedit:'',
       userageedit: '',
       useremailedit:'',
+
+      //DataAJAX
       userArticles: [],
       userForumposts:[],
       article:[],
+      forumpost:[],
 
-      //articleEdit
-    
       error:'',
-      categories:[
-        'Psychology',
-        'Social Life',
-        'Physical',
-        'Programming',
-        'Mistery',
-        'Others'
-      ],
-      tags:[],
-      // art: ''
+      //Others
+        categories:[
+          'Psychology',
+          'Social Life',
+          'Physical',
+          'Programming',
+          'Mistery',
+          'Others'
+        ],
+        tags:[],
     };
   },
   created() {
@@ -373,9 +445,11 @@ export default {
       this.friendsData = false
       this.articlesFavData = false
       this.articleInf = true
+      this.backButtonArt = true
+     
 
     },
-    saveArticle(e){
+    saveArticle(){
       // e.preventDefault();
       // let id = this.article.id_article
       let articleSaved = {
@@ -383,7 +457,7 @@ export default {
         textarticle: this.article.textarticle,
         autorarticle: this.user.username,
         categoryarticle: this.article.categoryarticle,
-        tagsarticle:  this.getTags(), 
+        tagsarticle:  this.getTagsArticles(), 
         id_user: this.article.id_user,
         id_article: this.article.id_article
       }
@@ -401,18 +475,96 @@ export default {
         }
       })   
     },
-    getTags(){
+    getTagsArticles(){
       let maptag = this.tags.map((tag) => {
         return tag.text
       })
       let stringTags = maptag.toString()
-      return stringTags + this.article.tagsarticle
+      return stringTags + this.article.tagsarticle  
+    },
+    getTagsForum(){
+      let maptag = this.tags.map((tag) => {
+        return tag.text
+      })
+      let stringTags = maptag.toString()
+      return stringTags + this.forumpost.tagsforumpost  
+    },
+    backButtonArticle(){
+      this.personalData = true
+      this.postsData = true
+      this.friendsData = true
+      this.articlesFavData = true
+      this.articleInf = false
+      this.backButtonArt = false
+    },
+    editForumpost(forumpost){
+      let id = forumpost
+      console.log(id)
+      axios.get('http://localhost:3000/forumpost/'+ id)
+      .then( 
+        (response) => {this.forumpost = response.data[0]}
+      )
+      .catch(error => {
+        if (!error.response) {
+            // network error
+            this.errorStatus = 'Error: Network Error';
+        } else {
+            this.errorStatus = error.response.data.message;
+        }
+      })
+      this.personalData = false
+      this.postsFavData = false
+      this.friendsData = false
+      this.articlesData = false
+
+      this.forumpostInf = true
+      this.backButtonForum = true
+    },   
+    saveForumpost(){
+      // e.preventDefault();
+      // let id = this.article.id_article
+      let forumpostSaved = {
+        titleforumpost: this.forumpost.titleforumpost,
+        textforumpost: this.forumpost.textforumpost,
+        autorforumpost: this.user.username,
+        categoryforumpost: this.forumpost.categoryforumpost,
+        tagsforumpost:  this.getTagsForum(), 
+        id_user: this.forumpost.id_user,
+        id_forumpost: this.forumpost.id_forumpost
+      }
+      axios.put('http://localhost:3000/saveForumpost', forumpostSaved)
+      
+      .then(
+        //  this.$router.push('/articles')       
+          console.log(forumpostSaved)
+      ).catch(error => {
+        if (!error.response) {
+            // network error
+            this.errorStatus = 'Error: Network Error';
+        } else {
+            this.errorStatus = error.response.data.message;
+        }
+      })   
+    },
+    backButtonForumpost(){
+      this.personalData = true
+      this.postsData = true
+      this.postsFavData = true
+      this.friendsData = true
+      this.articlesData = true
+
+      this.forumpostInf = false
+      this.backButtonForum = false
     }
   }
 };
 </script>
 
 <style scoped>
+
+.container-fluid{
+  background-color:#525252;
+}
 .t-align-left {
   text-align: left;
   padding-left: 1rem;
@@ -422,6 +574,11 @@ export default {
 }
 .margtop{
   margin-top:5rem !important;
+  margin-left:2.4rem !important;
+  border-radius: 5px 5px 5px 5px;
+}
+.margtop h1{
+   color:#5c5c5c;
 }
 
 .buttonEdit{
@@ -447,10 +604,14 @@ margin-bottom: 1rem;
 .bg-style{  
   background-color:#6ebe82;
 }
-.bg-white2{
+.bg-green2{
   background-color: rgb(0, 138, 96);
   padding: 0.2rem;
   color: rgb(216, 216, 216);
+  margin-top: 0.2rem;
+  border-radius: 50px 50px 50px 50px;
+  text-align: center;
+  
 }
 .barticle{
   float: right;
@@ -467,10 +628,16 @@ margin-bottom: 1rem;
 .vue-tags-input{
   max-width: 100%;
 }
-/* .margArt{
-  margin-left: 10rem;
- 
-} */
+.margArt{
+  margin-top: 1%;
+}
+.divsquery{
+  border-radius: 5px 5px 5px 5px;
+}
+.divsquery h2{
+  -webkit-text-stroke: medium;
+  border-radius: 5px 5px 5px 5px;
+}
 @media only screen and (max-width: 1000px) and (min-width: 5px) {
   .divsquery {
     margin-left: 0% !important;
@@ -483,6 +650,10 @@ margin-bottom: 1rem;
   .buttonEdit{
     position: relative;
     top: 0;
+  }
+  .margtop{
+    margin-top:4rem !important;
+    margin-left:0.2rem !important;
   }
 }
 </style>
